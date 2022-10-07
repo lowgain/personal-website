794e928e322f89e9e534aaa22d44381c0bc8d84e
Logan Thompson <logan.t2020@tutanota.com>
2022-10-07 12:22:51 -0400

    add bonsai

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 //
-// Make a number binary (byte size)
+// Make a number binary
 //
 
 let makeBin = (num, pad) => num.toString(2).padStart(pad, "0");
@@ -25,7 +25,19 @@ function displayDate() {
   if (hour > 12) { hour -= 12};
 
   document.getElementById("clock").innerHTML = makeBin(hour, 4) + ":" + makeBin(date.getMinutes(), 6);
-  document.getElementById("date").innerHTML = makeBin(date.getMonth(), 4) + ", " + makeBin(date.getDay(), 5);
+  document.getElementById("date").innerHTML = makeBin(date.getMonth() + 1, 4) + ", " + makeBin(date.getDate(), 5);
   document.getElementById("day").innerHTML = makeBin(date.getDay() + 1, 3);
 }
 timer(displayDate, 5000);
+
+//
+// Display a random bonsai plant
+//
+
+async function displayBonsai() {
+  let response = await fetch("./data.json");
+  let data = await response.json();
+
+  document.getElementById("bonsai").innerHTML = data.bonsais;
+}
+displayBonsai();
