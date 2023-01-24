7064cb1b7b63fdae56baea878ebab4b2d7856fcc
lowgain <logan.t2020@tutanota.com>
2023-01-24 16:41:10 -0500

    set clock to normal time

--- scripts.js
+++ scripts.js
@@ -16,18 +16,20 @@ function displayDate() {
   const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
   const weekdays = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat' ];
   const date = new Date();
-  const hour = date.getHours();
+  let hour = date.getHours();
   const minute = date.getMinutes().toString().padStart(2, '0');
   const month = months[date.getMonth()];
   const day = date.getDate();
   const weekday = weekdays[date.getDay()];
+  let amPm;
 
   if (hour == 0) { hour = 12 };
-  if (hour > 12) { hour -= 12};
+  if (hour > 12) { hour -= 12 };
+  if (hour < 12) { amPm = 'am' };
+  if (hour >= 12) { amPm = 'pm' };
 
-  $("clock").innerHTML = `${hour}:${minutes}`;
-  $("date").innerHTML = month + ", " + day;
-  $("day").innerHTML = weekday;
+  $("clock").innerHTML = `${hour}:${minute} ${amPm}`;
+  $("date").innerHTML = `${weekday} ${month}, ${day}`;
 }
 timer(displayDate, 5000);
 
