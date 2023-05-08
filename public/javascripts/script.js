8bd5d0b712259330705bb227dd7e3f5e06f1598f
lowgain <logan.t2020@tutanota.com>
2023-05-08 16:34:02 -0400

    update packages

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,58 +1,26 @@
 // Get Element
 const $ = element => document.getElementById(element);
 
-// Make a timer
-
-function timer(func, interval) {
-  func();
-  setInterval(func, interval);
-}
-
 //
 // format date and time display
 //
 
 function displayDate() {
-  const months = [ 'January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
-  const weekdays = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat' ];
   const date = new Date();
-  let hour = date.getHours();
-  const minute = date.getMinutes().toString().padStart(2, '0');
-  const month = months[date.getMonth()];
-  const day = date.getDate();
-  const weekday = weekdays[date.getDay()];
-  let amPm;
-
-  switch(true) {
-    case hour == 0:
-      hour = 12;
-      amPm = 'am';
-      break;
-    case hour < 12:
-      amPm = 'am';
-      break;
-    case hour > 12:
-      hour -= 12;
-      amPm = 'pm';
-      break;
-  }
-
-  $("clock").innerHTML = `${hour}:${minute} ${amPm}`;
-  $("date").innerHTML = `${weekday} ${month}, ${day}`;
+  $("clock").innerHTML = date.toLocaleTimeString();
+  $("date").innerHTML = date.toDateString();
 }
-timer(displayDate, 5000);
+displayDate();
+setInterval(displayDate, 1000);
 
 //
 // Display a random bonsai plant
 //
 
-function displayBonsai() {
-  const bonsais = [
-    "    #o#\n  ####o#\n #o# \\#|_#,#\n###\\ |/   #o#\n # {}{      #\n    }{{\n   ,'  `",
-    "    ,\\\n    # (_\n      _)\\##\n  ###/((_\n       ))\\####\n     _((\n####/  )\\\n     ,;;\"`;,",
-    "   ###o\n #o# \\#,_#,#\n###\\ |/   #o#\n # {}{      #\n    }{{\n   ,'  `"
-  ]
+const bonsais = [
+  "    #o#\n  ####o#\n #o# \\#|_#,#\n###\\ |/   #o#\n # {}{      #\n    }{{\n   ,'  `",
+  "    ,\\\n    # (_\n      _)\\##\n  ###/((_\n       ))\\####\n     _((\n####/  )\\\n     ,;;\"`;,",
+  "   ###o\n #o# \\#,_#,#\n###\\ |/   #o#\n # {}{      #\n    }{{\n   ,'  `"
+]
 
-  $("bonsai").innerHTML = bonsais[Math.floor(Math.random() * bonsais.length)];
-}
-displayBonsai();
+$("bonsai").innerHTML = bonsais[Math.floor(Math.random() * bonsais.length)];
