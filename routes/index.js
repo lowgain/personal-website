2da0f204d5f440acafc4a82c6d4160a6e3e643d4
lowgain <logan.t2020@tutanota.com>
2023-05-11 12:30:23 -0400

    add weather applet

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Startpage | Joneser Convention' });
+  res.render('index', { title: 'Startpage' });
 });
 
 module.exports = router;
